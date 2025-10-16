5341977fd38c781c7b7cfdb1d916feac54c36bf1
Marcos Ziadi <82457357+marcosziadi@users.noreply.github.com>
2025-10-16 16:01:49 -0300

    feat: add dimension customer builder

---
+++ src/etl/transform/build_dimension_customer.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def build_dim_customer(customer_clean_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Creates customer dimension.
+    """
+
+    return customer_clean_df    

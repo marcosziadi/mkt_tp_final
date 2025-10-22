3a6c05cf2c8328f787fd965c1f9ca09f70c6ac6d
Marcos Ziadi <82457357+marcosziadi@users.noreply.github.com>
2025-10-21 21:39:36 -0300

    feat: add web_session cleaning function and fact table builder

---
+++ src/etl/transform/build_fact_web_session_table.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+def build_fact_web_session(
+        web_session_clean: pd.DataFrame,
+        dim_customer: pd.DataFrame,
+        dim_device: pd.DataFrame,
+        dim_source: pd.DataFrame,
+        dim_calendar: pd.DataFrame
+    ) -> pd.DataFrame:
+    '''
+    DESCRIPTION
+    '''
+
+    fact_table = web_session_clean.copy()
+
+    fact_table = (
+        fact_table
+        .merge(dim_customer[['customer_key','customer_id']], on='customer_id', how="left")
+        .merge(dim_calendar[['time_key','datetime_id']].add_prefix("started_at_"), left_on='started_at_int', right_on='started_at_datetime_id', how="left")
+        .merge(dim_calendar[['time_key','datetime_id']].add_prefix("ended_at_"), left_on='ended_at_int', right_on='ended_at_datetime_id', how="left")
+        .merge(dim_source, on='source', how="left")
+        .merge(dim_device, on='device', how="left")
+        .drop(columns=['customer_id','started_at','ended_at','source','device'])
+        .rename(columns={'started_at_time_key': "started_at_key", 'ended_at_time_key': "ended_at_key"})
+    )
+
+    fact_table['session_key'] = fact_table.index + 1
+
+    fact_table = (
+        fact_table[[
+            'session_key',
+            'session_id',
+            'customer_key',
+            'started_at_key',
+            'ended_at_key',
+            'source_key',
+            'device_key',
+        ]]
+        .copy()
+    )
+
+    return fact_table
--- src/etl/transform/transform_web_session.py
+++ src/etl/transform/transform_web_session.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 CUSTOMER_UNKNOWN_ID = -1
-DATETIME_UNKNOWN = '1900-01-01 00:00:00'
+DATETIME_UNKNOWN_OBJ = '1900-01-01 00:00:00'
 
 def clean_web_session(web_session_raw: pd.DataFrame) -> pd.DataFrame:
     '''
@@ -14,13 +14,29 @@ def clean_web_session(web_session_raw: pd.DataFrame) -> pd.DataFrame:
         web_session_clean['started_at'] = pd.to_datetime(web_session_clean['started_at']).dt.floor('min')
         web_session_clean['ended_at'] = pd.to_datetime(web_session_clean['ended_at']).dt.floor('min')
 
-        web_session_clean['ended_at'] = web_session_clean['ended_at'].fillna(DATETIME_UNKNOWN)
         web_session_clean['customer_id'] = web_session_clean['customer_id'].fillna(CUSTOMER_UNKNOWN_ID)
+
+        web_session_clean['started_at_int'] = (
+            pd.to_datetime(web_session_clean['started_at'])
+            .fillna(str(DATETIME_UNKNOWN_OBJ))
+            .dt.strftime("%Y%m%d%H%M")
+            .astype(int)
+        )
+
+        web_session_clean['ended_at_int'] = (
+            pd.to_datetime(web_session_clean['ended_at'])
+            .fillna(str(DATETIME_UNKNOWN_OBJ))
+            .dt.strftime("%Y%m%d%H%M")
+            .astype(int)
+        )
         
         web_session_clean['duration_sec'] = (web_session_clean['ended_at'] - web_session_clean['started_at']).dt.total_seconds()
-        web_session_clean.loc[web_session_clean['ended_at' == DATETIME_UNKNOWN], 'duration_sec'] = -1
-    
+        web_session_clean.loc[web_session_clean['ended_at'] == DATETIME_UNKNOWN_OBJ, 'duration_sec'] = 0
+
+        return web_session_clean
+
+    except KeyError as e:
+        raise RuntimeError(f"ERROR: Failed cleaning web_session.csv | Missing required column: {e}") from e
+
     except Exception as e:
-        raise IOError('Failed cleaning web_session.csv')
-    
-    return web_session_clean
+        raise RuntimeError(f"ERROR: Failed cleaning web_session.csv | Unexpected Error: {e}") from e  

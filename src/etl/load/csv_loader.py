9d6e741872746366f7b76c61762ef0346c30bd86
Marcos Ziadi <82457357+marcosziadi@users.noreply.github.com>
2025-10-21 22:21:59 -0300

    feat: add data loader class

---
+++ src/etl/load/csv_loader.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+import pandas as pd
+
+class CSVLoader:
+    def __init__(self):
+        pass
+
+    def save_dataframe(self, output_path: Path, df: pd.DataFrame, filename: str):
+        """
+        Saves a dataframe as a csv file
+        """
+        file_path = output_path / filename
+
+        try:
+            output_path.mkdir(parents=True, exist_ok=True)
+            df.to_csv(f"{file_path}.csv", index = False)
+        except Exception as e:
+            raise IOError(f"Error saving file '{filename}' in {file_path}. Details: {str(e)}")
